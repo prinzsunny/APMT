08668c810f1a3d53c39adfe9061ce0ca0b94e877
LUVGOD <princemukka@Princes-MacBook-Pro.local>
2014-07-15 22:02:08 -0700

    Initial Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileDevelopment::Application.config.secret_token = '10cfae2cd6098a63f484d90a005ec09dec849913d12fb76d2d4dd62faaf442bf91f33da175728d0c10ef69a62c0c0891ae15b3162c08eb0e934cc79646fc47c8'
